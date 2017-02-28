b350c05264ef6380b19da844f11e683e5d6eddcd
Brian <blloyd08@uw.edu>
2017-02-27 22:08:38 -0800

    Add Views on Sql Script

--- Parking/Scripts/Imp Script.sql
+++ Parking/Scripts/Imp Script.sql
@@ -20,7 +20,6 @@ CREATE TABLE ParkingSpace (
 	spaceType VARCHAR(15)  NOT NULL REFERENCES SpaceType(`name`)
 );
 
-
 CREATE TABLE Staff (
 	staffID INT AUTO_INCREMENT PRIMARY KEY,
 	firstName VARCHAR(50) NOT NULL,
@@ -45,6 +44,44 @@ CREATE TABLE VisitorReservation (
 	licenseNumber VARCHAR(8) NOT NULL,
 	PRIMARY KEY (spaceID, reservedDay)
 );
+
+-- Views -----------------------------------------------------------------------------------------------------------------
+-- Current Reservations
+CREATE VIEW  CurrentStaffReservation AS (
+SELECT *
+FROM StaffReservation
+WHERE endDate > CURDATE());
+
+-- Staff Reservation --> Staff Drop Down
+-- Staff with no current reservations
+CREATE VIEW StaffWithoutReservation AS (
+SELECT *
+FROM Staff S
+WHERE S.staffID NOT IN (SELECT staffID FROM CurrentStaffReservation));
+
+
+SELECT *
+FROM CurrentStaffReservation
+-- Parking Space -> Lot Drop Down
+-- Parking Lots that aren't at capacity (Can create more spaces)
+CREATE VIEW UnfilledLots AS (
+SELECT L.lotName
+FROM ParkingLot L
+LEFT JOIN LotCounts S
+ON L.lotName = S.lotName
+WHERE capacity > Spaces OR Spaces IS NULL);
+
+-- Visitor Spots (Not checking for reservations)
+CREATE VIEW VisitorSpaces AS (
+SELECT spaceID
+FROM ParkingSpace
+WHERE spaceType = 'Visitor');
+
+-- Current number of spaces created for a parking lot
+CREATE VIEW LotCounts AS (
+SELECT lotName, COUNT(spaceID) 'Spaces'
+FROM ParkingSpace);
+
 -- INSERT STATEMENTS ----------------------------------------------------------------------------------------------------------------
 /*
 DEPENDENCE:
@@ -85,3 +122,35 @@ INSERT INTO ParkingSpace (lotName, spaceType) VALUES
 ('A','Covered'),
 ('A','Visitor'),
 ('A','Visitor');
+
+-- Spaces that aren't reserved 
+-- Staff Reservation -> Space Drop Down
+SELECT *
+FROM ParkingSpace
+WHERE spaceID NOT IN (
+	-- Reservations that haven't ended
+	SELECT spaceID
+	FROM CurrentStaffReservation
+)
+AND spaceType = "Covered";
+
+SELECT spaceID
+FROM CurrentStaffReservation
+
+
+SELECT *
+FROM ParkingSpace
+WHERE spaceID NOT IN (
+	SELECT spaceID
+	FROM StaffReservation
+	WHERE startDate < CURDATE())
+AND spaceType = "Covered";
+
+-- Currently Reserved Spaces
+SELECT *
+FROM StaffReservation
+WHERE endDate < CURDATE()
+
+-- Lot spaces
+SELECT lotName, COUNT(spaceID) 'Spaces'
+FROM ParkingSpace
